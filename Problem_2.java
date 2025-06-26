2aa3e20ed5dc72122bb8b4e20bf82da35688a21f
Aravindh <vaparavindh7@gmail.com>
2025-06-26 17:12:33 +0530

    second program completed

--- Problem_2.java
+++ Problem_2.java
@@ -1,3 +1,28 @@
+
+import java.util.Scanner;
+
 public class Problem_2 {
+    public static void main(String[] args) {
+        oddNumbers();
+    }
+    public static void oddNumbers(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int a = scan.nextInt();
+        scan.close();
+        if(a < 0){
+            System.out.println("enter the positive number");
+        }
+        int num = 1;
+        for(int i = 1; i <= a; i++){
+            if(i != 1){
+                System.out.print(", ");
+            }
+            System.out.print(num);
+            num  += 2;
+        }
+        
     
+     
+    }
 }

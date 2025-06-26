25a5b81ccc3425eb04e94c6af4fb2baebd82561f
Aravindh <vaparavindh7@gmail.com>
2025-06-26 17:20:21 +0530

    third problem finished

--- Problem_3.java
+++ Problem_3.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class Problem_3 {
+    public static void main(String[] args) {
+        oddNumbers();
+    }
+    public static void oddNumbers(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int a = scan.nextInt();
+        scan.close();
+        if(a < 0){
+            System.out.println("enter the positive number");
+            a = scan.nextInt();
+        }
+        int num = 1;
+        
+        //this is if  a is even number then it have to be odd number
+        if(a % 2 == 0){
+            a = a - 1;
+        }
+        
+        for(int i = 1; i <= a; i++){
+            if(i != 1){
+                System.out.print(", ");
+            }
+            System.out.print(num);
+            num  += 2;
+        }
+        
     
+     
+    }
 }
